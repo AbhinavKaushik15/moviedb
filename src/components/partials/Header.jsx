ed9d3229d4622c0b0cc2d72a1c6bdf758c79b7b7
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-01-18 19:59:39 +0530

    header

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -14,12 +14,12 @@ const Header = ({ data }) => {
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
-      className="w-full h-[55vh] sm:h-[65vh] p-10 flex flex-col justify-end gap-3"
+      className="w-full h-[55vh] sm:h-[65vh] p-10 flex flex-col justify-end gap-[1.3vw] sm:gap-[1vw]"
     >
-      <h1 className="text-white font-bold whitespace-nowrap text-[5vw] xl:text-[2.6vw] w-3/5">
+      <h1 className="text-white font-bold whitespace-nowrap text-[3.7vw] sm:text-[3.2vw] w-3/5">
         {data.title || data.original_title || data.name || data.original_name}
       </h1>
-      <p className="text-zinc-300 text-[2.45vw] sm:text-[1.4vw] xl:w-3/5">
+      <p className="text-zinc-300 text-[2.45vw] leading-[1.2] sm:text-[1.4vw] w-[60vw] sm:w-[45vw]">
         {data.overview.slice(0, 180)}...
         <Link
           to={`${data.media_type}/details/${data.id}`}
@@ -28,13 +28,13 @@ const Header = ({ data }) => {
           more
         </Link>
       </p>
-      <div className="flex gap-5">
-        <p className="text-white text-[2.5vw] xl:text-[1.2vw] flex items-center gap-2 justify-center">
+      <div className="flex gap-[2.8vw]">
+        <p className="text-white text-[2vw] sm:text-[1.2vw] flex items-center gap-2 justify-center">
           <HiOutlineSpeakerphone className="text-yellow-500" />
           {data.release_date || "Coming soon"}
         </p>
 
-        <p className="text-white text-[2.5vw] xl:text-[1.2vw] flex items-center justify-center gap-2">
+        <p className="text-white text-[2vw] sm:text-[1.2vw] flex items-center justify-center gap-2">
           <MdAlbum className="text-yellow-500" />
           {data.media_type.toUpperCase()}
         </p>
@@ -42,7 +42,7 @@ const Header = ({ data }) => {
 
       <Link
         to={`/${data.media_type}/details/${data.id}/trailer`}
-        className="bg-purple-600 hover:bg-white duration-300 w-fit py-1 px-2 text-[2vw] xl:text-[1.3vw] rounded-md text-white hover:text-purple-600"
+        className="bg-purple-600 hover:bg-white duration-300 w-fit py-[0.4vw] px-[0.8vw] text-[2vw] sm:text-[1.3vw] rounded-sm md:rounded-md text-white hover:text-purple-600"
       >
         Watch Trailer
       </Link>
