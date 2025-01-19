dca181ecd9f43ce788ae5b8adada3e544be8750c
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-01-19 14:03:09 +0530

    lollipop

--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -26,15 +26,15 @@ const Topnav = () => {
     <div className="w-full">
       <div className="fixed w-full sm:w-[81.45vw] h-[9vh] sm:h-[15vh] flex items-center gap-[1.8vh] sm:gap-5">
         <div className="flex items-center justify-center mx-auto gap-2">
-          <FiSearch className="text-[4.3vh] sm:text-[2vw] text-zinc-400" />
+          <FiSearch className="text-[3.5vh] sm:text-[2vw] text-zinc-400" />
           <input
             onChange={(e) => setquery(e.target.value)}
             value={query}
-            className="bg-transparent border-zinc-400 border-[0.5px] rounded-full outline-none py-[1vh] sm:py-[1vw] px-[1.5vh] sm:px-[2vw] w-[60vh] sm:w-[45vw] placeholder:text-zinc-400 text-white sm:placeholder:text-[1.5vw]"
+            className="bg-transparent border-zinc-400 border-[0.5px] rounded-full outline-none py-[1vh] sm:py-[1vw] px-[1.5vh] sm:px-[2vw] w-[50vh] sm:w-[45vw] placeholder:text-zinc-400 text-white sm:placeholder:text-[1.5vw]"
             type="text"
             placeholder="Search..."
           />
-          <div className="absolute w-[60vh] sm:w-[45vw] max-h-60 top-[88%] sm:top-[80%] overflow-auto">
+          <div className="absolute w-[50vh] sm:w-[45vw] max-h-60 top-[88%] sm:top-[80%] overflow-auto">
             {searches.map((s, i) => {
               return (
                 <Link
@@ -64,7 +64,7 @@ const Topnav = () => {
           {query.length > 0 && (
             <IoCloseOutline
               onClick={() => setquery("")}
-              className="ml-1 text-[6vh] sm:text-[2.6vw] text-zinc-400"
+              className="text-[6vh] sm:text-[2.6vw] text-zinc-400"
             />
           )}
         </div>
