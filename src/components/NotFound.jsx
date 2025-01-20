7d38584943ab9e405fb5441891416f26f68c304a
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-01-20 09:47:13 +0530

    404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import Found404 from "/Found404.gif";
 const NotFound = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#000]">
-      <img className="h-[100vh] object-cover" src={Found404} alt="" />
+      <img className="h-[40vh] sm:h-[100vh] object-cover" src={Found404} alt="" />
     </div>
   )
 }
